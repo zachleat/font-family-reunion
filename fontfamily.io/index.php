62ca40b23afe514c7f3f2767d363e3a124bbaebb
Zach Leatherman <zachleatherman@gmail.com>
2014-09-30 00:15:32 -0500

    Attribution

--- fontfamily.io/index.php
+++ fontfamily.io/index.php
@@ -42,6 +42,7 @@ $os = sanitize( basename( $_GET[ "os" ] ) );
 		</div>
 		<p class="note"><strong>Examples</strong>: <a href="/">(none set)</a> <a href="/sans-serif">sans-serif</a> <a href="/fantasy">fantasy</a> <a href="/cursive">cursive</a> <a href="/Andale_Mono,monospace">Andale Mono, monospace</a></p>
 		<p class="note">Per the specification, font-family names are case insensitive. <code>***</code> signifies a <code>font-family</code> that is only available by its aliased name in CSS.</p>
+		<p class="note"><em><a href="https://twitter.com/zachleat">—@zachleat</a></em></p>
 
 	</form>
 	<div class="results">
